d4c1b03a7adc4bcdda5463faf3ec165648d98ec5
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-21 08:09:46 +0800

    add titan one font, remove dark bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter, Titan_One } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const titan = Titan_One({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-        ></main>
+        >
+            <p className={`text-9xl uppercase ${titan.className}`}>Rosetta</p>
+        </main>
     );
 }
