9c1e6d57f8618bd03620afab9cee36fc3c43fbb9
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-21 23:08:19 +0800

    move fonts to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter, Titan_One } from "next/font/google";
+
+const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
+
+const titan = Titan_One({
+    weight: "400",
+    subsets: ["latin"],
+    variable: "--font-titan",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={`${inter.variable} ${titan.variable} font-inter`}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import { Inter, Titan_One } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-const titan = Titan_One({ weight: "400", subsets: ["latin"] });
-
 export default function Home() {
     return (
-        <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-        >
-            <p className={`text-9xl uppercase ${titan.className}`}>Rosetta</p>
-        </main>
+        <div className="flex flex-col justify-center items-center w-full min-h-screen">
+            <p className="text-9xl uppercase text-rosetta-orange font-titan">
+                Rosetta
+            </p>
+        </div>
     );
 }
