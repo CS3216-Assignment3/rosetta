f4169b04dd64f928b0e16df23c3e4fab58ef7399
tanruiquan <ruiquann@gmail.com>
2023-09-23 15:02:44 +0800

    Add Credentials login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,46 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "",
+          type: "text",
+          placeholder: "Email"
+        },
+        password: {
+          label: "",
+          type: "password",
+          placeholder: "Password"
+        }
+      },
+      async authorize(credentials) {
+        // This is where you need to retrieve user data 
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: "42", name: "John@example.com", password: "nextauth" }
+
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+            return user
+        } else {
+            return null
+        }
+      }
+    })
   ],
 }
 
