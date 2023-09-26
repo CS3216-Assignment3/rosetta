4939dbeda988424a8b5c635ee138d051ed7e89aa
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-26 20:41:05 +0800

    remove titan font

--- src/components/layouts/root-layout.tsx
+++ src/components/layouts/root-layout.tsx
@@ -1,12 +1,6 @@
 import NavBar from "@/components/navbar";
-import { Titan_One, Baloo_2 } from "next/font/google";
+import { Baloo_2 } from "next/font/google";
 import { ReactNode } from "react";
-
-const titan = Titan_One({
-    weight: "400",
-    subsets: ["latin"],
-    variable: "--font-titan",
-});
 
 const baloo = Baloo_2({
     subsets: ["latin"],
@@ -16,7 +10,7 @@ const baloo = Baloo_2({
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <main
-            className={`${baloo.variable} ${titan.variable} font-baloo bg-white text-rosetta-jet w-screen h-screen flex flex-col`}
+            className={`${baloo.variable} font-baloo bg-white text-rosetta-jet w-screen h-screen flex flex-col`}
         >
             <NavBar />
             {children}
