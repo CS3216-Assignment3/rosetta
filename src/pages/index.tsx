668446378f2c54b2a94a7e17abb5ff7c2e70d2ff
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-26 20:42:05 +0800

    homepage colours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,20 @@ export default function HomePage() {
         <div className="flex flex-col gap-12 items-center pt-24 w-full h-full">
             <p className="text-5xl font-bold text-center">
                 Learning languages one{" "}
-                <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rosetta-sienna to-rosetta-green">
-                    conversation{" "}
-                </span>
+                <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-rosetta-coral to-rosetta-orange">
+                    conversation
+                </span>{" "}
                 at a time.
             </p>
             <div className="grid grid-cols-2 gap-12 justify-center">
                 <Link
-                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-300 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-green"
+                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-300 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-orange"
                     href="/signin"
                 >
                     Sign In
                 </Link>
                 <Link
-                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-300 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-green"
+                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-300 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-orange"
                     href="/signup"
                 >
                     Sign Up
