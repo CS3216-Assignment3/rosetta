a6394cb3d76806f94148d4f0f30fb5d0f38d0e24
tanruiquan <ruiquann@gmail.com>
2023-09-28 02:12:23 +0800

    Add buffer memory for both bot

--- src/pages/api/message.ts
+++ src/pages/api/message.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import rosettaChatBot from '../../lib/langchain/ChatBot';
-import rosettaTutorBot from '../../lib/langchain/TutorBot';
+import ChatBot from '../../lib/langchain/ChatBot';
+import TutorBot from '../../lib/langchain/TutorBot';
 
 type ResponseData = {
     ok: boolean,
@@ -13,10 +13,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	if (req.method === 'POST') {
         try {            
             const { userId, chatId, text } = req.body
-            const { language, topic, proficiency } = getChatInfo()
+            const { language, topic, proficiency, chatHistory } = getChatInfo()
             const { nativeLanguage } = getUserInfo()
-            const chatResponse = await rosettaChatBot.sendMessage(topic, proficiency, language, text)
-            const tutorResponse = await rosettaTutorBot.evaluateMessage(language, nativeLanguage, proficiency, text)
+            const chatBot = new ChatBot(chatHistory)
+            const chatResponse = await chatBot.call(topic, proficiency, language, text)
+            const tutorBot = new TutorBot(chatHistory)
+            const tutorResponse = await tutorBot.call(language, nativeLanguage, proficiency, text)
             res.status(200).json({ ok: true, chatResponse, tutorResponse })
         } catch (error) {
             res.status(500).json({ ok: false, error: `An error occurred: ${error}.`})
@@ -28,7 +30,8 @@ function getChatInfo() {
     const language = "Chinese"
     const topic = "Gaming"
     const proficiency = "Beginner"
-    return { language, topic, proficiency } 
+    const chatHistory = [{ role: "ai", content: `你好, 你叫什么名字`}]
+    return { language, topic, proficiency, chatHistory } 
 }
 
 function getUserInfo() {
