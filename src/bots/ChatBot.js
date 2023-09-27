8708dbfa90db3da2819c75041b559a4b715c291b
tanruiquan <ruiquann@gmail.com>
2023-09-27 13:59:11 +0800

    Implement Chat and Tutor bot

--- src/bots/ChatBot.js
+++ src/bots/ChatBot.js
@@ -1,52 +1,66 @@
 import { inspect } from "util";
-import OpenAI from "openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ChatPromptTemplate } from "langchain/prompts";
 
 const MODEL = "gpt-3.5-turbo";
+const TEMPERATURE = 0.7
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+const chatModel = new ChatOpenAI({
+    model: MODEL,
+    temperature: TEMPERATURE
 });
 
-const CHAT_SYSTEM_PROMPT = `
-Act as a friendly and engaging friend from Germany that is interested in food.
-Respond as if you are conversing with a friend that is at a beginner level for German.
+const systemTemplate = `
+Act as a friendly and engaging friend that is interested in {topic}.
+Respond as if you are conversing with a friend that is at a {proficiency} level for {language}.
 
 Things to take note of while chatting with your friend:
-1. Chat and respond in German
+1. Chat and respond in {language}
 `;
 
+const humanTemplate = "{text}"
+
+const chatPrompt = ChatPromptTemplate.fromMessages([
+    ["system", systemTemplate],
+    ["human", humanTemplate],
+  ]);
+
 class ChatBot {
-    constructor(openai) {
-        this.openai = openai;
+    constructor(chatModel, chatPrompt) {
+        this.chatModel;
+        this.chatPrompt;
         this.messages = [
             { role: "system", content: CHAT_SYSTEM_PROMPT },
             { role: "assistant", content: GREETING },
         ];
     }
 
-    async sendMessage(msg) {
-        this.messages.push({
-            role: "user",
-            content: msg,
-        });
+    async sendMessage(topic, proficiency, language, text) {
+        const formattedChatPrompt = await this.chatPrompt.formatMessages({
+            topic,
+            proficiency,
+            language,
+            text,
+          });
 
-        const response = await this.openai.chat.completions.create({
-            model: MODEL,
-            messages: this.messages,
-        });
+        this.messages.push(formattedChatPrompt);
+
+        const chatModelResult = await this.chatModel.predictMessages(formattedChatPrompt);
 
         console.log(
             "chat response",
-            inspect(response, {
+            inspect(chatModelResult, {
                 showHidden: true,
                 depth: null,
                 colors: true,
             }),
         );
 
-        const responseMessage = response.choices[0].message;
-        this.messages.push(responseMessage);
+        this.messages.push(chatModelResult);
 
-        return response;
+        return chatModelResult;
     }
 }
+
+rosettaChatBot = new ChatBot(chatModel, chatPrompt);
+export default rosettaChatBot;
--- src/bots/TutorBot.js
+++ src/bots/TutorBot.js
@@ -1,52 +1,71 @@
 import { inspect } from "util";
-import OpenAI from "openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { ChatPromptTemplate } from "langchain/prompts";
+import rosettaChatBot from "./ChatBot";
 
 const MODEL = "gpt-3.5-turbo";
+const TEMPERATURE = 0.5
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+const chatModel = new ChatOpenAI({
+    model: MODEL,
+    temperature: TEMPERATURE
 });
 
-const TUTOR_SYSTEM_PROMPT = `
-Act as a kind, paitent and helpful tutor teaching German to English speaking students.
+const systemTemplate = `
+Act as a kind, paitent and helpful tutor teaching {language} to {nativeLanguage} speaking students.
 You will be presented with messages a student sent to their friend while chatting, which are delimited by triple quotes.
 
 Evaluate your student's messages by doing the following things:
 1. Check their grammar and offer suggestions on how to correct their message when it is wrong
 2. Check their vocabulary and phrases used, and offer better alternatives if needed
+3. Provide insights into cultural context, if appropriate
 
 Things to take note of while evaluating your student's message:
-1. Your student is learning German
-2. Your student speaks English, so respond in English
-3. Your student is at the beginner level for German
+1. Your student is learning {language}
+2. Your student speaks {nativeLanguage}, so respond in {nativeLanguage}
+3. Your student is at the {proficiency} level for {language}
 `;
 
+const humanTemplate = 'Please evaluate the message that I sent my friend """{text}"""'
+
+const tutorPrompt = ChatPromptTemplate.fromMessages([
+    ["system", systemTemplate],
+    ["human", humanTemplate],
+  ]);
+
 class TutorBot {
-    constructor(openai) {
-        this.openai = openai;
+    constructor(chatModel, tutorPrompt) {
+        this.chatModel = chatModel;
+        this.tutorPrompt = tutorPrompt;
+        this.log = []
     }
 
-    async evaluateMessage(msg) {
-        const response = await this.openai.chat.completions.create({
-            model: MODEL,
-            messages: [
-                { role: "system", content: TUTOR_SYSTEM_PROMPT },
-                {
-                    role: "user",
-                    content: `Please evaluate the message I sent my German friend: """${msg}"""`,
-                },
-            ],
-        });
+    async evaluateMessage(language, nativeLanguage, proficiency, text) {
+        const formattedTutorPrompt = await this.tutorPrompt.formatMessages({
+            language,
+            nativeLanguage,
+            proficiency,
+            text,
+          });
+
+        this.log.push(formattedTutorPrompt)
+
+        const chatModelResult = await this.chatModel.predictMessages(formattedTutorPrompt);
 
         console.log(
             "tutor response",
-            inspect(response, {
+            inspect(chatModelResult, {
                 showHidden: true,
                 depth: null,
                 colors: true,
             }),
         );
 
-        return response;
+        this.log.push(chatModelResult)
+
+        return chatModelResult;
     }
 }
+
+rosettaTutorBot = new TutorBot(chatModel, tutorPrompt);
+export default rosettaTutorBot;
