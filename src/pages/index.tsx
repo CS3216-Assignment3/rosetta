9cd58e38cf0f50a7a2686e0cf7dde5daf14c5c1b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-29 11:54:04 +0800

    landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import { useAuth } from "@/lib/auth/context";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import HomeHeroSection from "@/components/home-hero-section";
+import HomeFeaturesSection from "@/components/home-features-section";
+import HomePricingSection from "@/components/home-pricing-section";
 
 export default function HomePage() {
     const { loading, user } = useAuth();
@@ -14,28 +13,10 @@ export default function HomePage() {
     }, [loading, user]);
 
     return (
-        <div className="flex flex-col gap-12 items-center pt-24 w-full h-full">
-            <p className="text-5xl font-bold text-center">
-                Learning languages one{" "}
-                <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-rosetta-coral to-rosetta-orange">
-                    conversation
-                </span>{" "}
-                at a time.
-            </p>
-            <div className="grid grid-cols-2 gap-12 justify-center">
-                <Link
-                    href="/signin"
-                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-150 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-orange"
-                >
-                    Sign In
-                </Link>
-                <Link
-                    href="/signup"
-                    className="py-4 px-8 text-xl font-bold text-center rounded-lg duration-150 ease-in-out hover:text-white text-rosetta-jet bg-rosetta-coral shadow-inset hover:shadow-inset2 hover:bg-rosetta-orange"
-                >
-                    Sign Up
-                </Link>
-            </div>
+        <div className="overflow-y-scroll w-full h-full snap-y snap-proximity">
+            <HomeHeroSection />
+            <HomeFeaturesSection />
+            <HomePricingSection />
         </div>
     );
 }
