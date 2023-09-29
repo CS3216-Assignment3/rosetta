8d1c86f91ecc1a671754e2041efc7c32e936dc0f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-29 14:30:40 +0800

    add query to redirect url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import HomeHeroSection from "@/components/home-hero-section";
 import HomeFeaturesSection from "@/components/home-features-section";
 import HomePricingSection from "@/components/home-pricing-section";
+import { useAuth } from "@/lib/auth/context";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function HomePage() {
     const { loading, user } = useAuth();
@@ -8,7 +11,7 @@ export default function HomePage() {
 
     useEffect(() => {
         if (!loading && user) {
-            router.push("/lobby");
+            router.push("/lobby?section=chats");
         }
     }, [loading, user]);
 
